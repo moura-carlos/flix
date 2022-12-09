5d98325dc999d073b226587ab9b670f15147de5b
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-12-09 14:12:14 -0800

    generate a Movie model use a migration to create a movies database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_064608) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_09_220219) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
