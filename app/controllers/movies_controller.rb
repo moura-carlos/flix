c837fe331f3ef32448cef3ab24011f90dd67b428
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-12-10 13:03:11 -0800

    Implemented code to allow users to create movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,12 +2,15 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
   def show
     @movie = Movie.find(params[:id])
   end
+
   def edit
     @movie = Movie.find(params[:id])
   end
+
   def update
     @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
@@ -16,6 +19,20 @@ class MoviesController < ApplicationController
       render :edit
     end
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, status: :see_other
+    else
+      render :new
+    end
+  end
+
   private
   def movie_params
     params.require(:movie).permit(:title, :rating, :total_gross, :description, :released_on)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "movies#index"
+=begin
   get '/movies' => 'movies#index'
+
+  get 'movies/new' => 'movies#new', as: 'new_movie'
   get '/movies/:id' => 'movies#show', as: "movie"
+
   get '/movies/:id/edit' => 'movies#edit', as: "edit_movie"
   patch '/movies/:id' => 'movies#update'
+
+  post '/movies' => 'movies#create', as: 'create_movie'
+=end
+  resources :movies
 end
