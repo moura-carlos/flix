9d9b8c60bd146944e7e29bb5146d6f9a60c224f8
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-12-09 20:57:39 -0800

    Created form to allow user to edit movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,19 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to @movie, status: :see_other
+    else
+      render :edit
+    end
+  end
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :total_gross, :description, :released_on)
+  end
 end
