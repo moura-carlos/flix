f38b2bc0d97306b397a9dfa51b2927845d4eb388
Carlos Moura <carloseduardo.adm26@gmail.com>
2022-12-12 07:15:26 -0800

    Created code to allow users to edit and delete  their accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
     with: /\S+@\S+/
   }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 10, allow_blank: true }
-
+  validates :username, format: {
+    with: /\A[A-Z0-9]+\z/i
+  }, uniqueness: { case_sensitive: false }
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
   end
